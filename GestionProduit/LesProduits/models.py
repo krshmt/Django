b5b7fa3a4c7e65d68b661eb79a45ad556f9294ac
Kris.hmt <kris.toure@etu.univ-orleans.fr>
2024-09-11 12:01:27 +0200

    mise a jour de models

--- GestionProduit/LesProduits/models.py
+++ GestionProduit/LesProduits/models.py
@@ -7,7 +7,11 @@ from django.db import models
 Produit : nom, code, etc.
 """
 class Product(models.Model):
-    name = models.CharField(max_length=100) code = models.IntegerField()
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, null=True, blank=True)
+    prixHT = models.DecimalField(max_digits=8, decimal_places=2)
+    date_creation = models.DateTimeField(blank=True, null=True)
+    status = models.ForeignKey('Status', on_delete=models.CASCADE)
     def __unicode__(self):
         return "{0} [{1}]".format(self.name, self.code)
 
@@ -15,6 +19,7 @@ class Product(models.Model):
 Déclinaison de produit déterminée par des attributs comme la couleur, etc.
 """
 class ProductItem(models.Model):
+    code = models.CharField(max_length=10, null=True, blank=True)
     color =models.CharField(max_length=100)
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     def __unicode__(self):
